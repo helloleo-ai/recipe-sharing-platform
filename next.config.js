43dab914d30ff5903ed1e6cae00d59a374ec55e7
Terros <augustin@terros.io>
2024-08-06 20:18:59 +0200

    deploy netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-images: {
+  images: {
     domains: ['gibbonsgazette.org', 'images.pexels.com'],
   },
+  basePath: isProd ? '/recipe-sharing-platform' : '',
+  assetPrefix: isProd ? '/recipe-sharing-platform/' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
