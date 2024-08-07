2cff491b2d9889df572b8634a344dc4976703723
Terros <augustin@terros.io>
2024-08-07 13:04:47 +0200

    sync

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   images: {
     domains: ['gibbonsgazette.org', 'images.pexels.com'],
   },
-  basePath: isProd ? '/recipe-sharing-platform' : '',
-  assetPrefix: isProd ? '/recipe-sharing-platform/' : '',
+  // basePath: isProd ? '/recipe-sharing-platform' : '',
+  // assetPrefix: isProd ? '/recipe-sharing-platform/' : '',
   trailingSlash: true,
 }
 
